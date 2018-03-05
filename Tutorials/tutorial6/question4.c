cbaebf6be32fa9374e79aaabfddfb03c3af859b9
michaelSetnyk <msetnyk@gmail.com>
2018-03-04 20:02:21 -0500

    finished tutorial 6 question 4

--- Tutorials/tutorial6/question4.c
+++ Tutorials/tutorial6/question4.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <semaphore.h>
-#define NUMBERS 7
+#define NUMBERS 10
 int buffer_len =5;
 int buffer[5] = {0};
 
@@ -12,50 +12,39 @@ sem_t sem;
 //add to buffer_len
 void* producer(void* arg){
   int* arr = (int*)arg;
-
   for (int i =0; i< NUMBERS;i++){
     //if too large wait for the consumer
     sem_wait(&sem);
-
-    if(buffer_len< 4){
-      printf("buffer_len too large\n");
-
+    if(buffer_len<5){
       buffer[i] = arr[i];
       buffer_len++;
       printf("Produced %d\n",buffer[i]);
 
-      float sleep_len = (rand() % 5) / 10.0;
-      printf("producer sleep: %f\n",sleep_len);
-      sleep((rand() % 5) / 10.0) ;
-    }
-    //wake up consumer
+      int sleep_len = rand() % 3;
+      sleep(sleep_len);
+    }    //wake up consumer
       sem_post(&sem);
-
+      printf("i: %d\n",i );
   }
   return 0;
 }
 
 void* consumer(void* arg){
   int consumed_cnt =0;
-  while(consumed_cnt<10){
+  while(consumed_cnt<NUMBERS){
     sem_wait(&sem);
-
     //wait and allow producer to go
     if(buffer_len>=0){
-      printf("buffer_len too small\n");
-
-      printf("consumer buffer_len: %d\n", buffer_len);
       printf("Consumed: %d\n", buffer[buffer_len]);
       buffer[buffer_len]=0;
       buffer_len--;
 
-      float sleep_len =(rand() % 5) / 10.0;
-      sleep(sleep_len) ;
+      int sleep_len = rand() % 3;
+
+    sleep(sleep_len) ;
       consumed_cnt++;
     }
     sem_post(&sem);
-
-
   }
   return 0;
 }
@@ -64,6 +53,9 @@ void* consumer(void* arg){
 int main(void){
   pthread_t consumer_thread;
   pthread_t producer_thread;
+  time_t t;
+  srand((unsigned) time(&t));
+
 
   //hold entered numbers
   int arr[NUMBERS];
@@ -73,9 +65,9 @@ int main(void){
     }
 
     buffer_len=0;
-    sem_init(&sem, 0, 0);
+    sem_init(&sem, 0, 1);
 
-    pthread_create(&producer_thread, NULL, &producer, arr);
+    pthread_create(&producer_thread, NULL, &producer, &arr);
     pthread_create(&consumer_thread, NULL, &consumer, NULL);
 
     pthread_join(producer_thread,NULL);
