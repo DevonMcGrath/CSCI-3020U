0f28ff7e0ea1faaab3d2568613c228bf676b2e35
michaelSetnyk <msetnyk@gmail.com>
2018-03-19 11:33:01 -0400

    application question 1 done

--- Tutorials/tutorial9/demo1.c
+++ Tutorials/tutorial9/demo1.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <omp.h>
+
 int main (int argc, char *argv[])
 {
     printf("This statement will always print.\n");
+    /*
+    * will only enter this block when openmp is linked properly to the program
+    */
     #ifdef _OPENMP
     printf("This statement will only print when compiled with \"-fopenmp\"\n");
     #endif
--- Tutorials/tutorial9/demo2.c
+++ Tutorials/tutorial9/demo2.c
@@ -4,16 +4,30 @@ int main (int argc, char *argv[])
 {
     int num = 0; // Watch out for race conditions!
     int thread_num, nthreads = 16;
+    /*
+    will set the number of threads
+    if OpenMP is liked
+    */
     #ifdef _OPENMP
     omp_set_num_threads(nthreads);
     #endif
     printf("Testing OpenMP, you should see each thread print...\n");
+
+    /*
+    tells the compiler that we are going to be using
+    a pre processor.  In this case we are telling the
+    compiler that this block is using the omp parallel pre-processor
+    */
     #pragma omp parallel
     {
-        thread_num = omp_get_thread_num();
-        
+
+      /*
+      tells the compiler that this block is
+      in the omp critical section
+      */
         #pragma omp critical
         {
+            thread_num = omp_get_thread_num();
             #ifdef _OPENMP
             num += 1; // THREAD SAFE INCREMENT
             printf("This thread = %d, num = %d\n", thread_num, num);
